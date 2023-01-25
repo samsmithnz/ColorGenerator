c9164423435cc5fd9810292dbd063cc0da11c00b
Sam Smith <samsmithnz@gmail.com>
2023-01-25 08:22:33 -0500

    created new model

--- src/ColorGenerator.Web/Models/IndexModel.cs
+++ src/ColorGenerator.Web/Models/IndexModel.cs
@@ -1,6 +1,16 @@
-﻿namespace ColorGenerator.Web.Models
+﻿using SixLabors.ImageSharp.PixelFormats;
+
+namespace ColorGenerator.Web.Models
 {
     public class IndexModel
     {
+        public IndexModel(byte[] imageByteArray, List<Rgb24> colors)
+        {
+            ImageByteArray = imageByteArray;
+            Colors = colors;
+        }
+
+        public byte[] ImageByteArray { get; set; }
+        public List<Rgb24> Colors { get; set; }
     }
 }
