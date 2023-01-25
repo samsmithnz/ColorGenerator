f0954a2c72123da7d53eea6900f1b8d1a852d346
Sam Smith <samsmithnz@gmail.com>
2023-01-24 20:45:13 -0500

    returning an image

--- src/ColorGenerator.Web/Controllers/HomeController.cs
+++ src/ColorGenerator.Web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats;
 using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.PixelFormats;
 using System.Diagnostics;
 
 namespace ColorGenerator.Web.Controllers
@@ -18,7 +19,19 @@ namespace ColorGenerator.Web.Controllers
 
         public IActionResult Index()
         {
-            using Image image = Image.Load(Environment.CurrentDirectory + "/wwwroot/images/PuzzlePieces.jpg");
+            //Create the image with the target width and height
+            Image<Rgb24> newImage = new(500, 10);
+            //Add each pixel
+            for (int i = 0; i < 500; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    newImage[i, j] = new Rgb24(0, 0, 255);
+                }
+            }
+
+            //convert the image to a byte array and return it to the client
+            using Image image = newImage;// Image.Load(Environment.CurrentDirectory + "/wwwroot/images/PuzzlePieces.jpg");
             using (MemoryStream ms = new MemoryStream())
             {
                 IImageEncoder encoder = new JpegEncoder();
